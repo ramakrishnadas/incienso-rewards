859a1bc249ccc9dc719124f6107d7d0d5d21888c
RamaBhagi <rama.k.d.b.p@gmail.com>
2025-03-26 11:28:23 -0600

    move logout button to the right

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -3,5 +3,5 @@
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-  return <button className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline cursor-pointer" onClick={() => signOut()}>Logout</button>;
+  return <button className="absolute right-20 rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline cursor-pointer" onClick={() => signOut()}>Logout</button>;
 }
