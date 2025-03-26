ec5d57a0d5d6e4596b2254a4b58f48c7fa116bf9
RamaBhagi <rama.k.d.b.p@gmail.com>
2025-03-26 13:18:17 -0600

    fixed canje and movimiento form renderring

--- src/app/movimientos/[id]/page.tsx
+++ src/app/movimientos/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import CanjeForm from "@/app/components/CanjeForm";
 import MovimientoForm from "@/app/components/MovimientoForm";
-import { useParams, useSearchParams } from "next/navigation";
+import { useParams, useSearchParams, usePathname } from "next/navigation";
 
 export default function MovimientoPage() {
   const params = useParams(); 
@@ -11,21 +11,17 @@ export default function MovimientoPage() {
   const searchParams = useSearchParams();
   const clienteId = searchParams.get("cliente_id");
 
+  const pathname = usePathname();
+
   return (
     <div className="container mx-auto mt-6">
       {/* If cliente_id exists in the URL */}
-      {clienteId ? (
-        // If the URL is 'movimientos/canje', show CanjeForm
-        window.location.pathname.includes('canje') ? (
-          <CanjeForm clienteId={clienteId} />
+      {pathname.includes('canje') ? (
+          <CanjeForm clienteId={clienteId ?? ""} />
         ) : (
-          // If the URL is 'movimientos/nuevo', show MovimientoForm with clienteId
-          <MovimientoForm movimientoId={movimientoId === "nuevo" ? undefined : movimientoId} clienteId={clienteId} />
+          <MovimientoForm movimientoId={movimientoId === "nuevo" ? undefined : movimientoId} clienteId={clienteId ?? ""} />
         )
-      ) : (
-        // Default to MovimientoForm without clienteId
-        <MovimientoForm movimientoId={movimientoId === "nuevo" ? undefined : movimientoId} />
-      )}
+      }
     </div>
   );
 }
